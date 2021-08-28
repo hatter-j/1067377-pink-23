34085161124a44d0fe888c15c237d247535e79da
hatter-j <hatterjulia@mail.ru>
2021-08-28 20:17:45 +0300

    Адаптивная сетка для главной страницы

--- source/js/page-header-toggle.js
+++ source/js/page-header-toggle.js
@@ -2,5 +2,6 @@ var pageHeader = document.querySelector('.page-header');
 var navToggle = document.querySelector('.page-header__toggle');
 
 navToggle.addEventListener('click', function() {
+  pageHeader.classList.toggle('page-header--opened');
   pageHeader.classList.toggle('page-header--closed')
 });
